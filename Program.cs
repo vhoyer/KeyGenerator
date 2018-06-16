5870e8675d07bc9515c3bb2b1a4862d09317b5cd
Vinícius Hoyer <vhoyer@live.com>
2018-06-16 10:40:10 -0300

    Progress: running at ~100ms

--- Program.cs
+++ Program.cs
@@ -7,20 +7,21 @@ namespace KeyGenerator {
 	class Program {
 		static System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
 
+		//
+		// Parameters
+		//
 		static readonly char[] valid = "0123456789abcdefghijklmnopqrstuvxywzABCDEFGHIJKLMNOPQRSTUVXYWZ".ToCharArray();
 		static string lastKey = String.Format("{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}", valid[0]);
-
 		static int match = 25000000;
-		static double lastIndex = 0;
-		static double keysFound = 0;
-
 		static string datpath;
+		static double step = 1000000;
 
 		//
 		// runtime variables
 		//
 		static byte currentUsingValidIndex = 0;
-
+		static double lastIndex = 0;
+		static double keysFound = 0;
 		static void Main(string[] args) {
 			if(args.Length == 1) {
 				match = int.Parse(args[0]);
@@ -58,16 +59,12 @@ namespace KeyGenerator {
 			byte[] i = Array.ConvertAll(lastKey.ToCharArray(), x => (byte)x);
 			i = iterateThrough(i);
 
-			Func<int> calc = () => ((i[0] + i[1] + i[2] + i[3]) * (i[4] + i[5] + i[6] + i[7]) + (i[8] + i[9] + i[10] + i[11]) * (i[12] + i[13] + i[14] + i[15])) * (i[16] + i[17] + i[18] + i[19]);
-			Func<bool> test = () => calc() == match;
-
 			double j,maxIter = Math.Pow(256, 20) - 1;
 			for(j = lastIndex + 1; j < maxIter; j++) {
-				if(j % 1000000 == 0) {
-					Console.Write("\n{0}\t: {1}\t{2}%\t={3}\t[{4}ms] keys:{5}", compile(i), j, (j / maxIter * 100), calc(), stopwatch.ElapsedMilliseconds, keysFound);
-					stopwatch.Restart();
+				if(j % step == 0) {
+					printProgress(i, j, maxIter);
 				}
-				if(j % 10000000 == 0) {
+				if(j % (step*10) == 0) {
 					double progressJ = j,
 						   progressFound = keysFound;
 					string progressKey = compile(i);
@@ -77,11 +74,16 @@ namespace KeyGenerator {
 						Console.Write("\t✓");
 					}).Start();
 				}
-				if(test()) break;
+				if(calc(i) == match) break;
 				i = iterateThrough(i);
 			}
 			lastIndex = j;
 			return compile(i);
+		}
+
+		private static void printProgress(byte[] i, double j, double maxIter){
+            Console.Write("\n{0}\t: {1}\t{2}%\t={3}\t[{4}ms] keys:{5}", compile(i), j, (j / maxIter * 100), calc(i), stopwatch.ElapsedMilliseconds, keysFound);
+            stopwatch.Restart();
 		}
 
 		private static byte[] iterateThrough(byte[] i) {
@@ -110,5 +112,9 @@ namespace KeyGenerator {
 		private static string compile(byte[] i){
 			return String.Join("", Array.ConvertAll(i, x => (char)x));
 		}
+
+		private static double calc(byte[] i){
+			return ((i[0] + i[1] + i[2] + i[3]) * (i[4] + i[5] + i[6] + i[7]) + (i[8] + i[9] + i[10] + i[11]) * (i[12] + i[13] + i[14] + i[15])) * (i[16] + i[17] + i[18] + i[19]);
+		} 
 	}
 }
